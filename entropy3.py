aa5bb5b3b60fbf5db95ff404ea0503cca4e8711f
vkang6378 <91505648+vkang6378@users.noreply.github.com>
2023-12-03 14:46:16 -0500

    Update entropy3.py

--- entropy3.py
+++ entropy3.py
@@ -21,8 +21,22 @@ def is_high_entropy(s, threshold=3.5):
     """
     return calculate_entropy(s) > threshold
 
-def find_high_entropy_strings(file_path, threshold=3.0, min_length=10):
+def matches_api_pattern(s):
+    """
+    Check if the string matches known API key patterns.
+    """
+    return any(re.match(pattern, s) for pattern in api_key_patterns)
+
+def find_high_entropy_strings(source, is_url=False, threshold=3.0, min_length=10):
     high_entropy_strings = []
+
+    if is_url:
+        req = requests.get(source)
+        content = req.text.splitlines()
+    else:
+        with open(source, 'r') as file:
+            content = file.readlines()
+
 
     with open(file_path, 'r') as file:
         for line in file:
@@ -30,12 +44,25 @@ def find_high_entropy_strings(file_path, threshold=3.0, min_length=10):
 
             for word in words:
                 if len(word) >= min_length and all(c in string.ascii_letters + string.digits + "-_=" for c in word):
-                    entropy = calculate_entropy(word)
-                    if entropy > threshold:
+                    if is_high_entropy(word, threshold) or matches_api_pattern(word):
                         high_entropy_strings.append(word)
 
     return high_entropy_strings
 
+
+"""
+for testing entropy
 file_path = '/Users/jueunkang/test.py'
 high_entropy_strings = find_high_entropy_strings(file_path)
 print("High entropy strings (potential API keys):", high_entropy_strings)
+"""
+
+
+""""
+in scan.py:
+from entropy import find_high_entropy_strings
+
+inside "def fetch_github_data"
+high_entropy_strings = find_high_entropy_strings(repo_url)
+output_area.insert('end', f"\nHigh Entropy Strings (potential API keys):\n{high_entropy_strings}\n")
+"""
