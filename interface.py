9a6b8db8a386f551edf5ea8b6428dc8755da8a8e
jonathanm-12 <mikalovjonathan@gmail.com>
2023-12-04 16:27:40 -0500

    add new gui template, modify scan logic

--- interface.py
+++ interface.py
@@ -1,14 +1,67 @@
 import tkinter as tk
-from tkinter import scrolledtext
-from scan import fetch_github_data  # Importing the function
+from tkinter import ttk
+import random
 
+# Function to simulate scanning and generate fake results
+def scan():
+    username = username_entry.get()
+    ml_test = ml_var.get()
+    entropy_test = entropy_var.get()
+    regex_test = regex_var.get()
+
+    # Simulating fake repositories
+    fake_repositories = ["Repo1", "Repo2", "Repo3"]
+
+    # Generating fake scan results for each repository
+    results = [f"Scanning GitHub repositories of {username}..."]
+    for repo in fake_repositories:
+        results.append(f"\nResults for {repo}:")
+        if ml_test:
+            results.append(f"  ML Test: {random.randint(0, 100)} API Keys detected")
+        if entropy_test:
+            results.append(f"  Entropy Test: {random.randint(0, 100)} API Keys detected")
+        if regex_test:
+            results.append(f"  Regex Test: {random.randint(0, 100)} API Keys detected")
+
+    # Displaying the results
+    result_text = "\n".join(results) if results else "No tests selected"
+    result_label.config(text=result_text)
+    print(result_text)
+
+# Create the main window
 root = tk.Tk()
-root.title("GitHub Repository Viewer")
+root.title("GitHub Scanning Tool")
 
-output_area = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=10)
-output_area.pack(pady=10)
+# Create a frame for the input fields
+frame = ttk.Frame(root, padding="10")
+frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
-fetch_button = tk.Button(root, text="Fetch GitHub Data", command=lambda: fetch_github_data(output_area))
-fetch_button.pack()
+# GitHub username entry
+username_label = ttk.Label(frame, text="GitHub Username:")
+username_label.grid(row=0, column=0, sticky=tk.W)
+username_entry = ttk.Entry(frame, width=30)
+username_entry.grid(row=0, column=1)
 
+# Variables for the checkbuttons
+ml_var = tk.BooleanVar()
+entropy_var = tk.BooleanVar()
+regex_var = tk.BooleanVar()
+
+# Test options (ML, Entropy, Regex)
+ml_check = ttk.Checkbutton(frame, text="Machine Learning (ML) Test", variable=ml_var)
+ml_check.grid(row=1, column=0, columnspan=2, sticky=tk.W)
+entropy_check = ttk.Checkbutton(frame, text="Entropy Test", variable=entropy_var)
+entropy_check.grid(row=2, column=0, columnspan=2, sticky=tk.W)
+regex_check = ttk.Checkbutton(frame, text="Regex Test", variable=regex_var)
+regex_check.grid(row=3, column=0, columnspan=2, sticky=tk.W)
+
+# Scan button
+scan_button = ttk.Button(frame, text="Scan", command=scan)
+scan_button.grid(row=4, column=0, columnspan=2)
+
+# Label for displaying results
+result_label = ttk.Label(frame, text="")
+result_label.grid(row=5, column=0, columnspan=2)
+
+# Start the GUI
 root.mainloop()
--- scan.py
+++ scan.py
@@ -1,31 +1,38 @@
 import requests
 
 
-def fetch_github_data(output_area):
 
-    #private key undisclosed 
-    pat = ''
-    repo_url = 'https://api.github.com/repos/jonathanm-12/EC521-Final-Project/contents'
-    branch_url = 'https://api.github.com/repos/jonathanm-12/EC521-Final-Project/branches'
-    auth = ('jonathanm-12', pat)
+#private key undisclosed 
+pat = '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'
+repo_url = 'https://api.github.com/repos/jonathanm-12/EC521-Final-Project/contents'
+branch_url = 'https://api.github.com/repos/jonathanm-12/EC521-Final-Project/branches'
+auth = ('jonathanm-12', pat)
 
-    #authenticate response request using github key
-    req = requests.get(repo_url,auth=auth)
+#authenticate response request using github key
+req = requests.get(repo_url,auth=auth)
 
-    file_names = [item['name'] for item in req.json()]
+file_names = [item['name'] for item in req.json()]
 
-    req = requests.get('https://api.github.com/repos/jonathanm-12/EC521-Final-Project/branches', auth=auth)
+req = requests.get('https://api.github.com/repos/jonathanm-12/EC521-Final-Project/branches', auth=auth)
 
-    names = [item['name'] for item in req.json()]
-    main_branch = requests.get(branch_url, auth=auth).json()[0]['name']
+names = [item['name'] for item in req.json()]
+main_branch = requests.get(branch_url, auth=auth).json()[0]['name']
 
-    #file_content = []
+#file_content = []
+
+for i in file_names:
+    #append file content
+    #https://raw.githubusercontent.com/jonathanm-12/EC521-Final-Project/main/APIKEY_regex.txt
+    file_content = requests.get(f'https://raw.githubusercontent.com/jonathanm-12/EC521-Final-Project/{main_branch}/{i}', auth=auth)
     
-    for i in file_names:
-        #append file content
-        #https://raw.githubusercontent.com/jonathanm-12/EC521-Final-Project/main/APIKEY_regex.txt
-        file_content = requests.get(f'https://raw.githubusercontent.com/jonathanm-12/EC521-Final-Project/{main_branch}/{i}', auth=auth).content
-        output_area.insert('end', file_content)
-        output_area.insert('end', '\n')
+    if file_content.status_code != 404:
+        file_content = file_content.content
+    else:
+        continue
+    
+    #output_area.insert('end', file_content)
+    #output_area.insert('end', '\n')
+    
+    print(file_content)
 
 
