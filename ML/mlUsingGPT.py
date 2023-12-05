a3a09272804792ddb862ea763fbb2cd80a540d2a
Wiktor Lidwin <31045748+WiktorLidwin@users.noreply.github.com>
2023-12-04 22:00:15 -0500

    Add files via upload

--- ML/mlUsingGPT.py
+++ ML/mlUsingGPT.py
@@ -37,12 +37,11 @@ def load_json(file_path):
     return data
 def main():
     # Define the dataset
-    positive_data = load_json('true.json')
+    positive_data = load_json('keys2.json')
     negative_data = load_json('false.json')
     # Assign labels: positive=1, negative=0
     positive_labels = [1] * len(positive_data)
     negative_labels = [0] * len(negative_data)
-
     # Combine data and labels
     data = positive_data + negative_data
     labels = positive_labels + negative_labels
@@ -53,19 +52,22 @@ def main():
     data_with_labels = list(zip(data, labels))
     random.shuffle(data_with_labels)
 
-    train_data = data_with_labels[:int(0.8 * len(data))]
-    test_data = data_with_labels[int(0.8 * len(data)):]
-    print(train_data[0])
-    print(train_data[0:2])
-    dataset = train_data
+    train_data = data_with_labels[:int(0.05 * len(data))]
+    test_data = data_with_labels[int(0.02 * len(data)):int(0.03 * len(data))]
+   
+    # dataset = train_data
     # Load and tokenize the pre-trained GPT2 model
     tokenizer = GPT2Tokenizer.from_pretrained("distilgpt2")
     tokenizer.pad_token = tokenizer.eos_token
 
-    model = GPT2ForSequenceClassification.from_pretrained("distilgpt2")
-
+    model = GPT2ForSequenceClassification.from_pretrained("distilgpt2",output_hidden_states=True, )
+    model.config.label2id = {"LABEL_0": 0, "LABEL_1": 1}  # Replace with your label mapping
+    model.config.id2label = {0: "LABEL_0", 1: "LABEL_1"}  # Replace with your label mapping
+    model.config.num_labels = 2  # Replace with the number of labels in your task
     # Preprocess the dataset and tokenize the input text
-    tokenized_dataset = CustomDataset(dataset, tokenizer)
+    tokenized_dataset = CustomDataset(train_data, tokenizer)
+    eval_tokenized_dataset = CustomDataset(test_data, tokenizer)
+    
 
     # Define the fine-tuning training arguments
     training_args = TrainingArguments(
@@ -75,7 +77,7 @@ def main():
         per_device_train_batch_size=1,
         per_device_eval_batch_size=1,
         gradient_accumulation_steps=2,
-        num_train_epochs=3,
+        num_train_epochs=1,
         weight_decay=0.01
     )
 
@@ -84,35 +86,21 @@ def main():
         model=model,
         args=training_args,
         train_dataset=tokenized_dataset,
+        eval_dataset=eval_tokenized_dataset
     )
 
     # Fine-tune the model
     trainer.train()
-
+    def save_model(model, tokenizer, output_dir):
+        # Save model weights
+        model.save_pretrained(output_dir)
+        
+        # Save tokenizer
+        tokenizer.save_pretrained(output_dir)
     # Save the fine-tuned model for later use
-    trainer.save_model("fine_tuned_gpt_model")
-    
-
-    # model = GPT2ForSequenceClassification.from_pretrained("fine_tuned_gpt_model")
-
-    # # Load the tokenizer
-    # tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-
-    # # Prepare the input sequence
-    # input_sequence = "This"
-
-    # # Tokenize the input sequence
-    # inputs = tokenizer.encode_plus(input_sequence, add_special_tokens=True, padding="max_length", truncation=True, return_tensors="pt")
-    # input_ids = inputs['input_ids']
-    # attention_mask = inputs['attention_mask']
-
-    # # Perform the inference
-    # outputs = model(input_ids=input_ids, attention_mask=attention_mask)
-    # logits = outputs.logits
-    # predictions = logits.argmax(dim=1)
-
-    # # Print the predicted label
-    # print("Predicted Label:", predictions.item())
+    trainer.save_model("fine_tuned_gpt_model2")
+    output_dir = "fine_tuned_gpt_model3"
+    save_model(model, tokenizer, output_dir)
 
 if __name__ == "__main__":
     main()
